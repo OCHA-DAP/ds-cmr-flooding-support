1a9945ea29f8e1eab0db953eb4fe5f1df0b39149
t-downing <downing.tristan@gmail.com>
2025-01-29 14:34:59 -0800

    seas5 analysis

--- src/constants.py
+++ src/constants.py
@@ -5,3 +5,18 @@ ADM1_FLOOD_PCODES = [EXTREMENORD, NORD]
 
 LOGONE_CHARI = "CM004002"
 MAYO_DANAY = "CM004003"
+
+FRENCH_MONTHS = {
+    "Jan": "jan.",
+    "Feb": "fév.",
+    "Mar": "mars",
+    "Apr": "avr.",
+    "May": "mai",
+    "Jun": "juin",
+    "Jul": "juil.",
+    "Aug": "août",
+    "Sep": "sept.",
+    "Oct": "oct.",
+    "Nov": "nov.",
+    "Dec": "déc.",
+}
